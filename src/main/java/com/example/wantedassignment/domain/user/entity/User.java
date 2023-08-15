9d24606a2370983bbbf826d358cf5ceae3cfd7f3
goodzk <ihs221176@gmail.com>
2023-08-15 20:24:25 +0900

    feat: user 엔티티 추가

---
+++ src/main/java/com/example/wantedassignment/domain/user/entity/User.java
@@ -0,0 +1,52 @@
+package com.example.wantedassignment.domain.user.entity;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Column
+    private String roles;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, updatable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @PrePersist
+    public void prePersist() {
+        this.roles = "ROLE_USER";
+    }
+
+    @Builder
+    public User(Long id, String email, String password, String name, String roles) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.roles = roles;
+    }
+}
