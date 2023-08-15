9f25352f0d0707aabd23a11933d19b96a36d5f9a
goodzk <ihs221176@gmail.com>
2023-08-15 20:19:05 +0900

    feat: 애플리케이션 추가

---
+++ src/test/java/com/example/wantedassignment/WantedAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wantedassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
